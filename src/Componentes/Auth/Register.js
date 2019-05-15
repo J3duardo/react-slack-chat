195216808618c4f662cea0fbf0d04a820813a9b5
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-15 10:03:16 -0400

    Agregado link para ir al componente login"
    En caso de que el usuario ya esté registrado puede navegar directamente a la página de login para iniciar sesión"

--- src/Componentes/Auth/Register.js
+++ src/Componentes/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Header, Form, Segment, Button, Message, Icon, GridColumn} from "semantic-ui-react";
+import {Link} from "react-router-dom";
 
 class Register extends Component {
   onChangeHandler = () => {
@@ -16,17 +17,48 @@ class Register extends Component {
           </Header>
           <Form size="large">
             <Segment stacked>
-              <Form.Input fluid name="username" type="text" icon="user" iconPosition="left" placeholder="Username" onChange={this.onChangeHandler}/>
+              <Form.Input
+                fluid name="username"
+                type="text"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.onChangeHandler}
+              />
 
-              <Form.Input fluid name="email" type="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.onChangeHandler}/>
+              <Form.Input
+                fluid name="email"
+                type="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.onChangeHandler}
+              />
 
-              <Form.Input fluid name="password" type="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.onChangeHandler}/>
+              <Form.Input
+                fluid name="password"
+                type="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.onChangeHandler}
+              />
 
-              <Form.Input fluid name="passwordConfirmation" type="password" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={this.onChangeHandler}/>
+              <Form.Input
+                fluid name="passwordConfirmation"
+                type="password"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.onChangeHandler}
+              />
 
               <Button color="orange" fluid size="large">Submit</Button>
             </Segment>
           </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
         </GridColumn>
       </Grid>
     );
