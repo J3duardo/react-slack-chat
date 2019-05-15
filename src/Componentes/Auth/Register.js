c7065b22092196c16cc9732051bd186965249d65
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-15 09:59:41 -0400

    Agregado el formulario de registro de nuevos usuarios

--- src/Componentes/Auth/Register.js
+++ src/Componentes/Auth/Register.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {Grid, Header, Form, Segment, Button, Message, Icon, GridColumn} from "semantic-ui-react";
 
 class Register extends Component {
+  onChangeHandler = () => {
+
+  }
+
   render() {
     return (
       <Grid textAlign="center" verticalAlign="middle">
@@ -10,6 +14,19 @@ class Register extends Component {
             <Icon name="puzzle piece"/>
             Register for DevChat
           </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" type="text" icon="user" iconPosition="left" placeholder="Username" onChange={this.onChangeHandler}/>
+
+              <Form.Input fluid name="email" type="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.onChangeHandler}/>
+
+              <Form.Input fluid name="password" type="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.onChangeHandler}/>
+
+              <Form.Input fluid name="passwordConfirmation" type="password" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={this.onChangeHandler}/>
+
+              <Button color="orange" fluid size="large">Submit</Button>
+            </Segment>
+          </Form>
         </GridColumn>
       </Grid>
     );
