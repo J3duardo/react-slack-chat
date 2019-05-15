4dc4ed4776621ecf49ab7d5fe1811fbf1f2732e7
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-15 09:51:00 -0400

    Agregado el header del componente Register

--- src/Componentes/Auth/Register.js
+++ src/Componentes/Auth/Register.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {Grid, Header, Form, Segment, Button, Message, Icon, GridColumn} from "semantic-ui-react";
 
 class Register extends Component {
   render() {
     return (
-      <div>
-        Register
-      </div>
+      <Grid textAlign="center" verticalAlign="middle">
+        <GridColumn style={{maxWidth: 450}}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece"/>
+            Register for DevChat
+          </Header>
+        </GridColumn>
+      </Grid>
     );
   }
 }
