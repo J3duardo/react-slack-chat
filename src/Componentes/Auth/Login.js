f250d311b144e0867c2e6e44031ff52f40d92ad6
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-16 09:22:34 -0400

    Estructura básica de la página de Login
    El contenido básico de la página de login se copió en gran parte de la página de registro con algunas modificaciones.

--- src/Componentes/Auth/Login.js
+++ src/Componentes/Auth/Login.js
@@ -1,11 +1,99 @@
 import React, { Component } from 'react';
+import {Grid, Header, Form, Segment, Button, Message, Icon, GridColumn} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+import "./Register.css";
+import firebase from "../../firebase";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    errors: [],
+    loading: false
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+    if(this.isFormValid(this.state)) {
+      this.setState({errors: [], loading: true})
+    }
+  }
+
+  isFormValid = (state) => {
+    return state.email && state.password;
+  }
+
+  displayError = (errors) => {
+    return errors.map((error, i) => {
+      return <p key={i}>{error.message}</p>
+    })
+  }
+
+  inputErrorClassName = (errs, inputName) => {
+    return errs.some(err => err.message.toLowerCase().includes(inputName)) ? "error" : ""
+  }
+
   render() {
+    const {email, password, errors, loading} = this.state;
+
     return (
-      <div>
-        Login
-      </div>
+      <Grid textAlign="center" verticalAlign="middle" className="registerPage">
+        <GridColumn style={{maxWidth: 450}}>
+          <Header as="h1" icon color="violet" textAlign="center">
+            <Icon name="code branch"/>
+            Login to DevChat
+          </Header>
+          <Form size="large" onSubmit={this.onSubmitHandler}>
+            <Segment stacked>
+              <Form.Input
+                fluid name="email"
+                className={this.inputErrorClassName(errors, "email")}
+                type="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                value={email}
+                onChange={this.onChangeHandler}
+              />
+
+              <Form.Input
+                fluid name="password"
+                className={this.inputErrorClassName(errors, "password")}
+                type="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                value={password}
+                onChange={this.onChangeHandler}
+              />
+
+              <Button
+                color="violet"
+                fluid size="large"
+                disabled={loading}
+                className={loading ? "loading" : ""}
+              >
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayError(errors)}
+            </Message>
+          )}
+          <Message>
+            Don't have an account? <Link to="/register">Register</Link>
+          </Message>
+        </GridColumn>
+      </Grid>
     );
   }
 }
