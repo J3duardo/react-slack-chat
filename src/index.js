3ff359a33e1c1fc463821ad67bce20b2ae8bef89
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-16 11:03:57 -0400

    Refactorizado el index a componente stateful

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './Componentes/App';
 import Login from "./Componentes/Auth/Login";
@@ -8,16 +8,18 @@ import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-const ComponenteRoot = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </BrowserRouter>
-  )
+class ComponenteRoot extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
 ReactDOM.render(<ComponenteRoot />, document.getElementById('root'));
