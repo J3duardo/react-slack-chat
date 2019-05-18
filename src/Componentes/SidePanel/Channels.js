530187657417b692ab031fbadae6d34168c10468
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-18 08:31:45 -0400

    Creado el componente Channels

---
+++ src/Componentes/SidePanel/Channels.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {Menu, Icon} from "semantic-ui-react";
+
+class Channels extends Component {
+  state = {
+    channels: []
+  }
+
+  render() {
+    return (
+      <Menu.Menu style={{paddingBottom: "2rem"}}>
+        <Menu.Item>
+          <span>
+            <Icon name="exchange"/>
+          </span>
+          ({this.state.channels.length}) <Icon name="add"/>
+        </Menu.Item>
+      </Menu.Menu>
+    );
+  }
+}
+
+export default Channels;
