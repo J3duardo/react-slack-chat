ef6a7b28674095f8226e9995929e5854f0b10100
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-19 10:31:45 -0400

    Se agrega el nuevo mensaje y toda su información a la base de datos

--- src/Componentes/Messages/MessagesForm.js
+++ src/Componentes/Messages/MessagesForm.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import {Segment, Button, Input} from "semantic-ui-react";
+import {connect} from "react-redux";
+import firebase from "../../firebase";
 
 class MessagesForm extends Component {
   state = {
-    message: ""
+    message: "",
+    loading: false,
+    errors: []
   }
 
   onChangeHandler = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
+  }
+
+  sendMessage = () => {
+    if (this.state.message) {
+      this.setState({loading: true});
+      this.props.messagesRef
+        .child(this.props.channelId)
+        .push()
+        .set({
+          timestamp: firebase.database.ServerValue.TIMESTAMP,
+          content: this.state.message,
+          user: {
+            id: this.props.currentUser.uid,
+            name: this.props.currentUser.displayName,
+            avatar: this.props.currentUser.photoURL
+          }
+        })
+        .then(() => {
+          this.setState({
+            loading: false,
+            message: "",
+            errors: []
+          })
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({
+            loading: false,
+            errors: [...this.state.errors, err]
+          })
+        })
+    } else {
+      this.setState({
+        errors: [...this.state.errors, {message: "Add a message"}]
+      })
+    }
   }
 
   render() {
@@ -30,6 +70,7 @@ class MessagesForm extends Component {
             content="Add reply"
             labelPosition="left"
             icon="edit"
+            onClick={this.sendMessage}
           />
           <Button
             color="teal"
@@ -43,4 +84,11 @@ class MessagesForm extends Component {
   }
 }
 
-export default MessagesForm;
+const mapStateToProps = (state) => {
+  return {
+    channelId: state.currentChannel ? state.currentChannel.id : "Loading...",
+    currentUser: state.user.currentUser ? state.user.currentUser : "Loading..."
+  }
+}
+
+export default connect(mapStateToProps)(MessagesForm);
