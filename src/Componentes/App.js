ac7d8dfd9e44ad1d4bea0b956a2ae0ed821317d4
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-19 16:57:07 -0400

    Se pasa el usuario y el canal como props al componente Messages

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -6,13 +6,18 @@ import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import './App.css';
 
-const App = () => {
+import {connect} from "react-redux";
+
+const App = (props) => {
   return (
     <Grid columns="equal" className="app" style={{margin: 0, backgroundColor: "#eee"}}>
       <ColorPanel />
       <SidePanel />
       <Grid.Column style={{marginLeft: 320}}>
-        <Messages />
+        <Messages
+          user={props.user}
+          channel={props.channel}
+        />
       </Grid.Column>
       <Grid.Column>
         <MetaPanel width={4}/>
@@ -21,4 +26,11 @@ const App = () => {
   )
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+    channel: state.currentChannel.currentChannel
+  }
+}
+
+export default connect(mapStateToProps)(App);
