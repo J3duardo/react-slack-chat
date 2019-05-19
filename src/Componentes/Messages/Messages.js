be470a3f1f845ffd712de4b440850b012c5fbe59
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-19 17:04:37 -0400

    Se cargan los mensajes de la base de datos en el componente Messages

--- src/Componentes/Messages/Messages.js
+++ src/Componentes/Messages/Messages.js
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import {Segment, Comment} from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessagesForm from "./MessagesForm";
+import firebase from "../../firebase";
 
 class Messages extends Component {
+  state = {
+    channel: null,
+    user: null,
+    messagesRef: firebase.database().ref("messages")
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps !== this.props) {
+      this.setState({
+        channel: this.props.channel,
+        user: this.props.user
+      })
+    }
+
+    if(this.state.channel && this.state.user) {
+      this.addListeners(this.state.channel.id)
+    }
+  }
+
+  addListeners = (id) => {
+    this.addMessageListener(id);
+  }
+
+  addMessageListener = (id) => {
+    const loadedMessages = [];
+    this.state.messagesRef.child(id).on("child_added", (snap) => {
+      loadedMessages.push(snap.val());
+      console.log(loadedMessages)
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
