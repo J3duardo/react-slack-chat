9fdde98bc9581ba8a2f53be966e6557dbe7af637
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-20 09:01:16 -0400

    Agregado modal para adjuntar imágenes en los mensajes

---
+++ src/Componentes/Messages/FileModal.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Modal, Input, Button, Icon} from "semantic-ui-react";
+
+class FileModal extends Component {
+  render() {
+    return (
+      <Modal basic open={this.props.modal} onClose={this.props.closeModal}>
+        <Modal.Header>Select an image file</Modal.Header>
+        <Modal.Content>
+          <Input
+            fluid
+            label="File types: .jpg, .png"
+            type="file"
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="green" inverted>
+            <Icon name="checkmark"/> Send
+          </Button>
+          <Button color="red" inverted onClick={this.props.closeModal}>
+            <Icon name="remove"/> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
+
+export default FileModal;
