84cf95bfeb2ada5117bb2946762daae0acd98176
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-30 17:01:01 -0400

    Se muestran los canales seleccionados como favoritos
    Los canales seleccionados como favoritos se muestran en el panel lateral

--- src/Componentes/SidePanel/Starred.js
+++ src/Componentes/SidePanel/Starred.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import {Menu, Icon} from "semantic-ui-react";
+import {connect} from "react-redux";
+import {setCurrentChannel, setPrivateChannel} from "../../actions";
 
 class Starred extends Component {
   state = {
-    starredChannels: []
+    starredChannels: [],
+    activeChannel: ""
   }
-  
+
+  renderChannels = (starredChannels) => {
+    if (starredChannels.length > 0) {
+      return starredChannels.map((channel) => {
+        return (
+          <Menu.Item
+            key={channel.id}
+            onClick={() => this.changeChannel(channel)}
+            name={channel.name}
+            style={{opacity: 0.7}}
+            active={channel.id === this.state.activeChannel}
+          >
+            <span><Icon name="angle right"/> {channel.name}</span>
+          </Menu.Item>
+        )
+      })
+    } else {
+      return <p style={{fontSize: "1rem", color: "#fff"}}>No channels to display.</p>
+    }
+  }
+
+  setActiveChannel = (channel) => {
+    this.setState({
+      activeChannel: channel.id
+    })
+  }
+
+  changeChannel = (channel) => {
+    this.props.currentChannel(channel);
+    this.setActiveChannel(channel);
+    this.props.setPrivateChannel(false);
+  }
+
   render() {
     return (
       <Menu.Menu style={{paddingBottom: "2rem"}}>
@@ -13,12 +48,23 @@ class Starred extends Component {
             <span>
               <Icon name="star"/> Starred
             </span>{" "}
-            ({this.state.starredChannels.length}) <Icon name="add" onClick={this.openModal}/>
+            ({this.state.starredChannels.length})
           </Menu.Item>
-          {this.renderChannels(this.state.channels)}
+          {this.renderChannels(this.state.starredChannels)}
         </Menu.Menu>
     );
   }
 }
 
-export default Starred;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCurrentChannel: (channel) => {
+      dispatch(setCurrentChannel(channel))
+    },
+    setActiveChannel: (bool) => {
+      dispatch(setPrivateChannel(bool))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Starred);
