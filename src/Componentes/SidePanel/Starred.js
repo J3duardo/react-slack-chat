c39cc4080b39a5f1bba3edb7e000f913f2cd807c
J3duardo <jesus_69eduardo@hotmail.com>
2019-06-01 09:49:24 -0400

    Se muestran los canales favoritos en el panel lateral

--- src/Componentes/SidePanel/Starred.js
+++ src/Componentes/SidePanel/Starred.js
@@ -2,11 +2,50 @@ import React, { Component } from 'react';
 import {Menu, Icon} from "semantic-ui-react";
 import {connect} from "react-redux";
 import {setCurrentChannel, setPrivateChannel} from "../../actions";
+import firebase from "../../firebase";
 
 class Starred extends Component {
   state = {
     starredChannels: [],
-    activeChannel: ""
+    activeChannel: "",
+    usersRef: firebase.database().ref("users")
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.currentUser !== prevProps.currentUser) {
+      this.addListeners(this.props.currentUser.uid)
+    }
+  }
+
+  addListeners = (userId) => {
+    this.state.usersRef
+    .child(userId)
+    .child("starred")
+    .on("child_added", snapshot => {
+      const starredChannel = {
+        id: snapshot.key,
+        ...snapshot.val()
+      };
+      this.setState({
+        starredChannels: [...this.state.starredChannels, starredChannel]
+      })
+    });
+
+    this.state.usersRef
+    .child(userId)
+    .child("starred")
+    .on("child_removed", snapshot => {
+      const channelToRemove = {
+        id: snapshot.key,
+        ...snapshot.val()
+      };
+      const filteredChannels = this.state.starredChannels.filter(channel => {
+        return channel.id !== channelToRemove.id
+      });
+      this.setState({
+        starredChannels: filteredChannels
+      });
+    });
   }
 
   renderChannels = (starredChannels) => {
@@ -36,23 +75,29 @@ class Starred extends Component {
   }
 
   changeChannel = (channel) => {
-    this.props.currentChannel(channel);
     this.setActiveChannel(channel);
+    this.props.setCurrentChannel(channel);
     this.props.setPrivateChannel(false);
   }
 
   render() {
     return (
       <Menu.Menu style={{paddingBottom: "2rem"}}>
-          <Menu.Item>
-            <span>
-              <Icon name="star"/> Starred
-            </span>{" "}
-            ({this.state.starredChannels.length})
-          </Menu.Item>
-          {this.renderChannels(this.state.starredChannels)}
-        </Menu.Menu>
+        <Menu.Item>
+          <span>
+            <Icon name="star"/> Starred
+          </span>{" "}
+          ({this.state.starredChannels.length})
+        </Menu.Item>
+        {this.renderChannels(this.state.starredChannels)}
+      </Menu.Menu>
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser
   }
 }
 
@@ -61,10 +106,10 @@ const mapDispatchToProps = (dispatch) => {
     setCurrentChannel: (channel) => {
       dispatch(setCurrentChannel(channel))
     },
-    setActiveChannel: (bool) => {
+    setPrivateChannel: (bool) => {
       dispatch(setPrivateChannel(bool))
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(Starred);
+export default connect(mapStateToProps, mapDispatchToProps)(Starred);
